57130a9be748a637bf2e8b4927c5c18156e19e8c
moe <moe@email.com>
2023-12-22 03:57:48 -0500

    designing the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage'
 import AuthPage from './pages/Authpage/AuthPage'
+import PageLayout from './Layouts/PageLayout/PageLayout'
 function App () {
   return (
-    <>
+    <PageLayout>
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/auth' element={<AuthPage />} />
       </Routes>
-    </>
+    </PageLayout>
   )
 }
 
