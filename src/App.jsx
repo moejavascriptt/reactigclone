84f2b8a019ae2c16ef8f751fce1373556f7bb87e
moe <moe@email.com>
2023-12-23 15:10:52 -0500

    designing prof  page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage'
 import AuthPage from './pages/Authpage/AuthPage'
 import PageLayout from './Layouts/PageLayout/PageLayout'
+import ProfilePage from './pages/ProfilePage/ProfilePage'
 function App () {
   return (
     <PageLayout>
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/auth' element={<AuthPage />} />
+        <Route path='/:username' element={<ProfilePage />} />
       </Routes>
     </PageLayout>
   )
